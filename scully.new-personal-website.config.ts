dbc0b6374f1d945a8b166865e2a0ac2cc721e2b7
Santiago Ruffino <santi_0898@me.com>
2020-11-06 14:26:37 -0300

    Styles in article. G Analytics

--- scully.new-personal-website.config.ts
+++ scully.new-personal-website.config.ts
@@ -1,11 +1,22 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { setPluginConfig, ScullyConfig, prod } from '@scullyio/scully';
+import { GoogleAnalytics } from '@scullyio/scully-plugin-google-analytics';
+
+const defaultPostRenderers = [];
+
+if (prod) { 
+  setPluginConfig(GoogleAnalytics, {globalSiteTag: 'G-9YBPCZYRSL'});
+  defaultPostRenderers.push(GoogleAnalytics);
+}
+
 export const config: ScullyConfig = {
+  defaultPostRenderers,
   projectRoot: "./src",
   projectName: "new-personal-website",
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
+      postRenderers: [...defaultPostRenderers],
       slug: {
         folder: "./blog"
       }
